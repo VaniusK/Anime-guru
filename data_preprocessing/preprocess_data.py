dee04e1a19d2ffa08f9eb12399a52dac2af9636a
Ivan Konobeitsev <konobeitsev3@gmail.com>
2024-12-19 16:46:57 +0300

    Removed anime without english name

--- data_preprocessing/preprocess_data.py
+++ data_preprocessing/preprocess_data.py
@@ -3,5 +3,6 @@ from imports import *
 anime_table=pd.read_csv('../anime-dataset-2023-with-common-tags.csv', usecols=lambda x: 'Unnamed' not in x)
 popularity_threshold = 50
 anime_table= anime_table.query('Members >= @popularity_threshold')
+anime_table = anime_table[anime_table['English name'] != "UNKNOWN"]
 anime_table= anime_table.reset_index(drop=True)
 anime_table.to_csv("../anime-dataset-2023-with-common-tags.csv")
